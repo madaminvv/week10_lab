874200bb4a6a175ed20b121743f066970d7acb64
250132 <250132@newuu.uz>
2025-10-30 12:56:58 +0500

    week 10

--- main.cpp
+++ main.cpp
@@ -18,4 +18,14 @@ int n;
         cout << avgNumInArray(the_numbers, n);
 
 }
-int n
+void sortArray(int arr[], int n) {
+        for (int i = 0; i < n - 1; ++i) {
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                      arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
